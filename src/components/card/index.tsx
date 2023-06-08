6c9ed2bf853cb80e5d894386c6d6bf1e2e9adb31
GilbertoFreitas997 <gilbertofreitas997@gmail.com>
2023-06-08 12:22:26 -0300

    fix(recipe-list): added grid-auto-rows so cards dont overlap

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,17 +2,16 @@ import { FC } from "react";
 import { CardProps } from "./types";
 
 export const Card: FC<CardProps> = (props) => {
-    const { header, footer, className, onClick } = props;
+  const { header, footer, className, onClick } = props;
 
-    return (
-        <div className={`w-full h-56 flex flex-col shadow-lg ${className}`} onClick={onClick}>
-            <div className="h-44 w-full">
-                {header}
-            </div>
+  return (
+    <div
+      className={`w-full h-full min-h-56 flex flex-col shadow-lg ${className}`}
+      onClick={onClick}
+    >
+      <div className="h-3/4 w-full">{header}</div>
 
-            <div className="flex-grow">
-                {footer}
-            </div>
-        </div>
-    )
-}
+      <div className="flex-grow">{footer}</div>
+    </div>
+  );
+};
--- src/components/recipe-list/index.tsx
+++ src/components/recipe-list/index.tsx
@@ -27,7 +27,7 @@ export const RecipeList = () => {
   ));
 
   return (
-    <div className="w-full grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-col-6 gap-3 mt-4">
+    <div className="w-full grid auto-rows-[_1fr] sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-col-6 gap-3 mt-4">
       {loading ? loadingTemplate : cards}
     </div>
   );
